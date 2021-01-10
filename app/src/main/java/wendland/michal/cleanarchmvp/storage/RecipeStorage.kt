07a5d3d3276350bf0a79ceeb0ad1301e15f82442
Michal Wendland <wendland.michal@gmail.com>
2021-01-10 15:14:26 +0100

    Add RecipeStorage

---
+++ app/src/main/java/wendland/michal/cleanarchmvp/storage/RecipeStorage.kt
@@ -0,0 +1,14 @@
+package wendland.michal.cleanarchmvp.storage
+
+import wendland.michal.cleanarchmvp.entity.Recipe
+
+class RecipeStorage() {
+    private lateinit var _recipes : MutableList<Recipe>
+
+    val getRecipe : MutableList<Recipe>
+        get() = _recipes
+
+    fun add(recipe: Recipe) {
+        _recipes.add(recipe)
+    }
+}
